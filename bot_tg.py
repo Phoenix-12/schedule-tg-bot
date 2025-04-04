89019b69a437808fb17430918a172869337eee22
Lindo-rx <lond1.aby@gmail.com>
2025-04-04 18:33:33 +0300

    Update bot_tg.py

--- bot_tg.py
+++ bot_tg.py
@@ -3,24 +3,49 @@ from datetime import datetime, timedelta
 import telebot
 from telebot import types
 import sqlite3
+import db_mysql
+
 bot = telebot.TeleBot('7959873800:AAH2pgOXXef0g6cm2fXLz54c38KZ-3TXb7E')
 conn = sqlite3.connect('users.db')
 cursor = conn.cursor()
+gusini = db_mysql.database_initialization()
+admin_list = ['hylo_x2']
+
 
 @bot.message_handler(commands=['start'])
 def start_command(message):
     a = f'@{message.from_user.username} здравствуйте!'
-    markup = types.ReplyKeyboardMarkup()
-    btn1 = types.KeyboardButton('Зарегистрироваться', )
-    btn2 = types.KeyboardButton('Войти')
-    markup.row(btn1, btn2)
-    bot.send_message(message.chat.id, a)
-    bot.send_message(message.chat.id, 'Вас приветствует ITeam помощник!', reply_markup=markup)
+    if a in admin_list:
+        markup = types.ReplyKeyboardMarkup()
+        btn3 = types.KeyboardButton('Массовая рассылка')
+        btn4 = types.KeyboardButton('Вывести всех пользователей')
+        markup.row(btn3, btn4)
+        bot.send_message(message.chat.id, a)
+        bot.send_message(message.chat.id, 'Вас приветствует ITeam помощник!', reply_markup=markup)
+        admin_func()
+
+    elif a not in admin_list:
+        markup = types.ReplyKeyboardMarkup()
+        btn1 = types.KeyboardButton('Зарегистрироваться')
+        btn2 = types.KeyboardButton('Войти')
+        markup.row(btn1, btn2)
+        bot.send_message(message.chat.id, a)
+        bot.send_message(message.chat.id, 'Вас приветствует ITeam помощник!', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def admin_func(message):
+    if message.text == 'Массовая рассылка':
+        send_handler()
+    elif message.text == 'Вывести всех пользователей':
+        cursor.execute('SELECT * FROM gusini')
+        users = cursor.fetchall()
+        users = str(users)
+        bot.send_message(message.chat.id, text=users)
 
 @bot.message_handler(content_types=['text'])
 def func(message):
-
-    if (message.text == 'Зарегистрироваться'):
+    if message.text == 'Зарегистрироваться':
 
         bot.send_message(message.chat.id, 'Сейчас тебя зарегистрируем! Введи своё имя:')
         bot.register_next_step_handler(message, user_name)
@@ -32,7 +57,7 @@ def func(message):
         bot.send_message(message.chat.id, text="Всё ок")
 
     elif (message.text == 'Войти'):
-        bot.send_message(message.chat.id, 'Сейчас тебя 100100100111001 в нашу программу! Введи своё имя:')
+        bot.send_message(message.chat.id, 'Инициализация протокола входа. Введи своё имя:')
         bot.register_next_step_handler(message, user_name)
     elif message.text == 'Посмотреть своё расписание':
         bot.send_message(message.chat.id, text="Всё ок")
@@ -48,20 +73,24 @@ def func(message):
 
     elif message.text == 'Информация по курсам':
         bot.send_message(message.chat.id, text="Всё ок")
+
+
 def user_name(message):
     global name
     name = message.text.strip()
-    bot.send_message(message.chat.id, 'Введи свой пароль:')
-    bot.register_next_step_handler(message, user_pass)
-def user_pass(message):
-    password = message.text.strip()
-    bot.send_message(message.chat.id, 'Введи свой номер телефона:')
-    bot.register_next_step_handler(message, user_p_n)
-def user_p_n(message):
-    p_n = message.text.strip()
+    bot.send_message(message.chat.id, 'Введи свою электронную почту:')
+    bot.register_next_step_handler(message, email_number)
+
+
+def email_number_tg(message):
+    tg_id = message.from_user.username
+    email_number = message.text.strip()
     bot.send_message(message.chat.id, 'Пользователь зарегистрирован (или успешно зашёл в свой аккаунт)')
     bot.register_next_step_handler(message, answer)
     answer(message)
+    db_mysql.new_user(name, email_number, tg_id)
+
+
 def answer(message):
     markup = types.ReplyKeyboardMarkup()
     btn1 = types.KeyboardButton("Посмотреть своё расписание")
@@ -70,6 +99,8 @@ def answer(message):
     markup.row(btn1)
     markup.row(btn2, btn3)
     bot.send_message(message.chat.id, text="Что хочешь сделать?", reply_markup=markup)
+
+
 def send_message_to_subscribers(message):
     cursor.execute('SELECT chat_id FROM users WHERE subscribed = 1')
     rows = cursor.fetchall()
@@ -79,6 +110,8 @@ def send_message_to_subscribers(message):
             bot.send_photo(message.chat.id, photo, caption='Ваш текст')
         except Exception as e:
             print(f'Ошибка в отправке сообщения {chat_id}: {e}')
+
+
 def send_notification(student_name, days_until_lesson):
     print(f"Напоминание для {student_name}: до начала урока осталось {days_until_lesson} дней.")
 
@@ -95,10 +128,10 @@ def notification_func(students_list: list):
         if delta == 3:
             send_notification(student["name"], delta)
 
-
     while True:
         notification_func(students)
         time.sleep(86400)
+
 
 @bot.message_handler(commands=['send'])
 def send_handler(message):
@@ -106,4 +139,8 @@ def send_handler(message):
         send_message_to_subscribers(message.text[6:])
     else:
         bot.send_message(message.chat.id, 'У вас нет прав на выполнение этой команды.')
+
+
+cursor.close()
+conn.close()
 bot.polling(none_stop=True)
